6300ff574aae05f10709508d3a9e53e2ca37ad9a
Siqi <me@siqi.fr>
2014-02-12 00:29:31 +0100

    new TestFlight tocken

--- ios/iosremote/iosremote/Classes/AppDelegate.m
+++ ios/iosremote/iosremote/Classes/AppDelegate.m
@@ -28,7 +28,7 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [TestFlight takeOff:@"29f8d6f9-de56-4866-bd70-15b5305f812e"];
+    [TestFlight takeOff:@"cb645cf2-a557-481e-993a-6ad3aaeb06d0"];
     
     NSString *defaultsPath = [[NSBundle mainBundle] pathForResource:@"UserDefaults"
                                                              ofType:@"plist"];
